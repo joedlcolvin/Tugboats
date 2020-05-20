7d2a95ba2c5ba8f10e7d8ce47f3ccae828782f0a
joedlcolvin <joefishbass@gmail.com>
2020-05-20 20:25:07 +0100

    Render episode working

--- learn.py
+++ learn.py
@@ -1,7 +1,36 @@
+import math
 import numpy as np
 import render
 from operator import add
 from queue import PriorityQueue
+
+class Environment:
+    def __init__(self, polygons, ship_length, grid_dims):
+        self.grid_dims = grid_dims
+        for poly in polygons:
+            assert(self._poly_in_grid(poly, grid_dims))
+        self.polygons = polygons
+        self.ship_length = ship_length
+
+    def ship_ends(self, state):
+        return [[state[0]+math.cos(math.radians(state[2]))*0.5*self.ship_length, 
+                 state[1]-math.sin(math.radians(state[2]))*0.5*self.ship_length],
+                [state[0]-math.cos(math.radians(state[2]))*0.5*self.ship_length, 
+                 state[1]+math.sin(math.radians(state[2]))*0.5*self.ship_length]]
+
+    def _poly_in_grid(self, poly, grid_dims):
+        b = True
+        for vertex in poly.v:
+            if vertex[0] >= grid_dims[0] or vertex[0] < 0 or vertex[1] >= grid_dims[1] or vertex[1] < 0:
+                b = False
+        return b
+
+class Polygon:
+    def __init__(self, v):
+        self.v = v
+        self.e = [[v[i],v[i+1]] for i in range(len(v)-1)]
+        self.e.append([v[len(v)-1],v[0]])
+        self.outside = [min([vert[0] for vert in v])-1, min([vert[1] for vert in v])-1]
 
 ### Learning parameters ###
 # GONNA NEED SOME TESTING
@@ -16,8 +45,8 @@ SHIP_LENGTH = 10
 #Define square grid
 GRID_WIDTH = 100
 GRID_HEIGHT = 100
-POLYGONS = [[[0,40],[50,40],[50,50],[0,50]],
-            [[40,50],[50,50],[50,80],[40,80]]]
+POLYGONS = [Polygon([[0,40],[50,40],[50,50],[0,50]]),
+            Polygon([[40,50],[50,50],[50,80],[40,80]])]
 ENV = Environment(POLYGONS, SHIP_LENGTH, (GRID_WIDTH, GRID_HEIGHT))
 ORIENT_VAL_NUM = 360    #One for each degree
 ACTION_NUM = 6          #Number of different actions = #{forward, back, left, right, acw, cw}
@@ -28,24 +57,35 @@ initial_state = [10,10,0] # states defined as tuples,
                         # [x position, y position, degrees anticlockwise from x-axis]
 termination_state = [38,65,270]
 
+### Render params ###
+WIN_WIDTH = 500
+WIN_HEIGHT= 500
+FRAME_TIME_MS = 30
+
 ### INITIALISING TABLE ###
 #Table for the q values
 #Dimensions are in order (x position, y position, orientation, action)
 #Actions are ordered as indicated on line 6
 #Note that here we are initialising our q_table with an optimistic expectation for most state-action pairs, since most incur negative rewards
 q_table = np.zeros(shape=(GRID_WIDTH, GRID_HEIGHT, ORIENT_VAL_NUM, ACTION_NUM), dtype=float)
-for i in range(GRID_WIDTH):
-    for j in range(GRID_HEIGHT):
-        for r in range(ORIENT_VAL_NUM):
-            for a in range(ACTION_NUM):
-                if(not is_valid(env_response([i,j,r],a)[1])):
-                    q_table[i,j,r,a] = -np.inf
 #Priority queue - implemented using collections library deque
 p_queue = PriorityQueue()
 
 def main():
+    init_q_table()
     learn_q()
     states, actions, rewards = run_pi()
+    px_scale = min(int(WIN_WIDTH/GRID_WIDTH), int(WIN_HEIGHT/GRID_HEIGHT))
+    episode = render.Episode(states, ENV, px_scale, [WIN_WIDTH,WIN_HEIGHT])
+    episode.animate(FRAME_TIME_MS)
+
+def init_q_table():
+    for i in range(GRID_WIDTH):
+        for j in range(GRID_HEIGHT):
+            for r in range(ORIENT_VAL_NUM):
+                for a in range(ACTION_NUM):
+                    if(not is_valid(env_response([i,j,r],a)[1])):
+                        q_table[i,j,r,a] = -np.inf
 
 #Note: Ensure that actions returned are legal
 def greedy_pol(S):
@@ -54,6 +94,7 @@ def greedy_pol(S):
 #TODO implement
 def epsilon_greedy_pol(S):
     #REMEMBER when choosing action, must check that it is legal
+    return -1
 
 def env_response(S,A):
     reward_func_list = [-10,-10,-100,-100,-1,-1]
@@ -80,7 +121,7 @@ def backup(S):
     reverse_action = [1,0,3,2,5,4]
 
     state_actions = []
-    for(a in range(6)):
+    for a in range(6):
         state = env_response(S,a)
         # REMEMBER only legal, non-terminal states should go in this list
         if(is_valid(state) or state == terminal_state):
@@ -93,7 +134,7 @@ def snap_to_grid(S):
 def learn_q():
     # LEARNING Q
     S = initial_state
-    for(i in range(ITER_NUM)):
+    for i in range(ITER_NUM):
         A = greedy_pol(S)   #TODO try epsilon greedy policy here
         R, Sprime = env_response(S,A)
         P = math.abs(R+GAMMA*max(q_table[Sprime[0],Sprime[1],Sprime[2]])-q_table[S[0],S[1],S[2],A])
@@ -104,7 +145,7 @@ def learn_q():
                 S, A = p_queue.pop()
                 R, Sprime = env_response(S,A)
                 q_table[S[0],S[1],S[2],A] = q_table[S[0],S[1],S[2],A] + ALPHA*(R+GAMMA*max(q_table[Sprime[0],Sprime[1],Sprime[2]])-q_table[S[0],S[1],S[2],A])
-                for(Sbar,Abar in backup(S)):
+                for Sbar,Abar in backup(S):
                     Rbar = env_response(Sbar,Abar)
                     P = math.abs(Rbar+GAMMA*max(q_table[S[0],S[1],S[2]])-q_table[Sbar[0],Sbar[1],Sbar[2],Abar])
                     if P > THETA:
@@ -115,7 +156,7 @@ def run_pi():
     actions = []
     rewards = []
     S = initial_state
-    while(S!=termination_state):
+    while S!=termination_state:
         state.append(S)
         A = greedy_pol(S)
         actions.append(A)
@@ -146,30 +187,5 @@ def is_valid(state, environment):
     def _intersect_points(self,A,B,C,D):
         return self._ccw(A,C,D) != self._ccw(B,C,D) and self._ccw(A,B,C) != self._ccw(A,B,D)
 
-class Environment:
-    def __init__(self, polygons, ship_length, grid_dims):
-        self.grid_dims = grid_dims
-        for poly in polygons:
-            assert(self._poly_in_grid(poly, grid_dims)
-        self.polygons = polygons
-        self.ship_length = ship_length
 
-    def ship_ends(self, state):
-        return [[state.x+math.cos(math.radians(state.r))*0.5*self.ship_length, 
-                 state.y-math.sin(math.radians(state.r))*0.5*self.ship_length],
-                [state.x-math.cos(math.radians(state.r))*0.5*self.ship_length, 
-                 state.y+math.sin(math.radians(state.r))*0.5*self.ship_length]]
 
-    def _poly_in_grid(poly, grid_dims):
-        b = True
-        for vertex in poly.v:
-            if vertex[0] => grid_dims[0] or vertex[0] < 0 or vertex[1] => grid_dims[1] or vertex[1] < 0:
-                b = False
-        return b
-
-class Polygon:
-    def __init__(self, v):
-        self.v = v
-        self.e = [[v[i],v[i+1]] for i in range(len(v)-1)]
-        self.e.append([v[len(v)-1],v[0]])
-        self.outside = [min([vert[0] for vert in v])-1, min([vert[1] for vert in v])-1]
--- render.py
+++ render.py
@@ -2,61 +2,43 @@ import graphics
 import math
 import time
 
-def render(win, environment, initial_state, px_scale):
-    for domain in environment.domains:
-        for i in range(len(domain.e)):
-            graphics.Line(  graphics.Point(domain.e[i][0][0]*px_scale, domain.e[i][0][1]*px_scale), 
-                            graphics.Point(domain.e[i][1][0]*px_scale, domain.e[i][1][1]*px_scale)).draw(win)
+def render(win, initial_state, px_scale, environment, enviro_rend=False):
+    if enviro_rend:
+        for polygon in environment.polygons:
+            for i in range(len(polygon.e)):
+                graphics.Line(  graphics.Point(polygon.e[i][0][0]*px_scale, polygon.e[i][0][1]*px_scale), 
+                                graphics.Point(polygon.e[i][1][0]*px_scale, polygon.e[i][1][1]*px_scale)).draw(win)
+
     ship = environment.ship_ends(initial_state)
-    print(ship)
     ship_rend = graphics.Line(  graphics.Point(ship[0][0]*px_scale, ship[0][1]*px_scale), 
                                 graphics.Point(ship[1][0]*px_scale, ship[1][1]*px_scale)).draw(win)
-
-    message = graphics.Text(graphics.Point(win.getWidth()/2, 20), 'Click anywhere to quit.')
-    message.draw(win)
-    win.getMouse()
-    win.close()
 
     return ship_rend
 
 
 class Episode:
-    def __init__(self, initial_state, actions, environment, px_scale, win_size):
-        current_state = initial_state
-        for action in actions:
-            assert(action.is_valid())
-            assert(current_state.is_valid(environment))
-            current_state = State(  current_state.x+action.x,
-                                    current_state.y+action.y,
-                                    current_state.r+action.r)
-        assert(current_state.is_valid(environment))
-
-        self.initial_state = initial_state
+    def __init__(self, states, environment, px_scale, win_size):
         self.environment = environment
-        self.actions = actions
+        self.states = states
         self.px_scale = px_scale
-        self.win_size
+        self.win_size = win_size
 
     def animate(self, delay):
-        win = GraphWin("Tugboat episode", win_size[0], win_size[1], autoflush=False)
+        win = graphics.GraphWin("Tugboat episode", self.win_size[0], self.win_size[1], autoflush=False)
+        ship_rend = render(win, self.states[0], self.px_scale, self.environment, True)
+        # We render ship twice on first state, but this doesn't matter
+        for state in self.states:
+            ship_rend.undraw()
+            ship_rend = render(win, state, self.px_scale, self.environment)
+            graphics.update(delay)
 
-        message = Text(Point(win.getWidth()/2, 20), 'Click anywhere to quit.')
+        message = graphics.Text(graphics.Point(win.getWidth()/2, 20), 'Click anywhere to quit.')
         message.draw(win)
         win.getMouse()
         win.close()
 
-        ship_rend = render(win, self.environment, self.initial_state, self.px_scale)
-        time.sleep(delay)
-        for action in self.actions:
-            if math.isclose(action.r, 0, rel_tol=1e-5):
-                ship_rend.move(math.cos(math.radians(state.r))*action.x, math.sin(math.radians(state.r))*action.x)
-            else:
-                #TODO IMPLEMENT REDRAWING OF SHIP USING graphics.undraw() and then graphics.draw()
-                pass
-            graphics.update(1/delay)
-
-def test():
-    from learn import Domain, Environment
+def test_static():
+    from learn import Polygon, Environment
 
     WIN_WIDTH = 500
     WIN_HEIGHT = 500
@@ -64,10 +46,28 @@ def test():
     SHIP_LENGTH = 10
     GRID_HEIGHT = 100       #Define square grid
     GRID_WIDTH = 100
-    POLYGONS = [[[0,40],[50,40],[50,50],[0,50]],
-                [[40,50],[50,50],[50,80],[40,80]]]
+    POLYGONS = [Polygon([[0,40],[50,40],[50,50],[0,50]]),
+                Polygon([[40,50],[50,50],[50,80],[40,80]])]
     ship_length = 10
     env = Environment(POLYGONS, SHIP_LENGTH, [GRID_WIDTH, GRID_HEIGHT])
     state = [10,10,0]
     px_scale = min(int(WIN_WIDTH/GRID_WIDTH), int(WIN_HEIGHT/GRID_HEIGHT))
-    render(win, env, state, px_scale)
+    render(win, state, px_scale, env)
+
+def test_episode():
+    from learn import Polygon, Environment
+
+    
+    WIN_WIDTH = 500
+    WIN_HEIGHT = 500
+    SHIP_LENGTH = 10
+    GRID_HEIGHT = 100       #Define square grid
+    GRID_WIDTH = 100
+    POLYGONS = [Polygon([[0,40],[50,40],[50,50],[0,50]]),
+                Polygon([[40,50],[50,50],[50,80],[40,80]])]
+    ship_length = 10
+    env = Environment(POLYGONS, SHIP_LENGTH, [GRID_WIDTH, GRID_HEIGHT])
+    states = [[10,10,i] for i in range(360)]
+    px_scale = min(int(WIN_WIDTH/GRID_WIDTH), int(WIN_HEIGHT/GRID_HEIGHT))
+    episode = Episode(states, env, px_scale, (WIN_WIDTH, WIN_HEIGHT))
+    episode.animate(30)
