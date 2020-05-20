8ed7f393ddb90b2aa018ea962af87dcd54cb311d
joedlcolvin <joefishbass@gmail.com>
2020-05-20 15:27:20 +0100

    Just a couple of things to change before testing

--- learn.py
+++ learn.py
@@ -1,10 +1,12 @@
 import numpy as np
-from collections import deque
+from queue import PriorityQueue
 
 # Learning parameters
+# GONNA NEED SOME TESTING
 GAMMA = 0.95
-THETA = #???
+THETA = 1.0
 ALPHA = 0.1
+ITER_NUM = 1e6          #Number of prioritised sweeping iterations
 
 # Environment parameters
 GRID_HEIGHT = 100       #Define square grid
@@ -19,29 +21,38 @@ TRANSLATION_DIST = 2
 #Actions are ordered as indicated on line 6
 q_table = np.zeros(shape=(GRID_WIDTH, GRID_HEIGHT, ORIENT_VAL_NUM, ACTION_NUM), dtype=float)
 #Priority queue - implemented using collections library deque
-p_queue = []
+p_queue = PriorityQueue()
 
 initial_state = [0,0,0] # states defined as tuples, 
                         # [x position, y position, degrees anticlockwise from x-axis]
+termination_state = [10,10,0]
 
-ITER_NUM = 1e6          #Number of prioritised sweeping iterations
-
-
-S = initial_state
-for(i in range(ITER_NUM)):
-    A = greedy_pol(S)
-    R, Sprime = env_response(S,A)
-    P = math.abs(R+GAMMA*max(q_table[Sprime[0],Sprime[1],Sprime[2]])-q_table[S[0],S[1],S[2],A)
-    if P > THETA:
-        p_queue.append(
+def main()
+    S = initial_state
+    for(i in range(ITER_NUM)):
+        A = greedy_pol(S)
+        R, Sprime = env_response(S,A)
+        P = math.abs(R+GAMMA*max(q_table[Sprime[0],Sprime[1],Sprime[2]])-q_table[S[0],S[1],S[2],A])
+        if P > THETA:
+            #Since priority queue only uses integer priority, we use P multiplied to 100 and rounded
+            p_queue.put(math.round(P*100), (S,A))
+            while(not p_queue.empty()):
+                S, A = p_queue.pop()
+                R, Sprime = env_response(S,A)
+                q_table[S[0],S[1],S[2],A] = q_table[S[0],S[1],S[2],A] + ALPHA*(R+GAMMA*max(q_table[Sprime[0],Sprime[1],Sprime[2]])-q_table[S[0],S[1],S[2],A])
+                for(Sbar,Abar in backup(S)):
+                    Rbar = env_response(Sbar,Abar)
+                    P = math.abs(Rbar+GAMMA*max(q_table[S[0],S[1],S[2]])-q_table[Sbar[0],Sbar[1],Sbar[2],Abar])
+                    if P > THETA:
+                        p_queue.put(math.round(P*100), (S,A))
 
 
 def greedy_pol(S):
     return np.argmax(qtable[S[0],S[1],S[2]])
 
 def env_response(S,A):
-    reward_func = [-10,-10,-100,-100,-1,-1]
-    #TODO
+    reward_func_list = [-10,-10,-100,-100,-1,-1]
+    reward_func = lambda Sprime,A: 0 if Sprime==termination_state else reward_func_list[A]
     action_func = [ lambda orient: ,    # move forward by TRANSLATION_DIST 
                     lambda orient: ,    # move back by TRANSLATION_DIST
                     lambda orient: ,    # move left by TRANSLATION_DIST
@@ -49,7 +60,11 @@ def env_response(S,A):
                     lambda orient: ,    # rotate 1 degree anti clockwise
                     lambda orient: ]    # rotate 1 degree clockwise
 
-    R = reward_func[A]
-    Sprime = snap_to_grid(S + action_func[A])
+    Sprime = snap_to_grid(S + action_func[A](S[2]))     #TODO change to use elementwise addition with mod 360 for angle
+    R = reward_func(Sprime,A)
 
     return R, Sprime
+
+def backup(S):
+    #TODO
+    return #list of (Sprime,A) which result in S
