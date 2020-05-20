2f51e1646321388ba3a0fc45c364df2480b2e527
joedlcolvin <joefishbass@gmail.com>
2020-05-20 14:16:28 +0100

    Starting to implement learning algo

--- learn.py
+++ learn.py
@@ -1,6 +1,16 @@
 import numpy as np
 
-GRID_HEIGHT = 100
+GRID_HEIGHT = 100       #Define square grid
 GRID_WIDTH = 100
+ORIENT_VAL_NUM = 360    #One for each degree
+ACTION_NUM = 6          #Number of different actions = #{up, down, left, right, acw, cw}
 
-coord = np.ndarray((GRID_HEIGHT, GRID_WIDTH, ))
+#Table for the q values
+q_table = np.zeros(shape=(GRID_HEIGHT, GRID_WIDTH, ORIENT_VAL_NUM, ACTION_NUM), dtype=float)
+#Priority queue - unknown length so using a list
+p_queue = []
+
+ITER_NUM = 1e6          #Number of prioritised sweeping iterations
+
+for(i in range(ITER_NUM)):
+    
